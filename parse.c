6dd1b876c1a991dcde5f1d2d493063499c8872d4
Jeffxzj <xuzj@shanghaitech.edu.cn>
2019-06-06 23:15:38 +0800

    add parsing pipe feature

--- parse.c
+++ parse.c
@@ -5,15 +5,17 @@
 
 void
 init_parseinfo(info_t *info) {
-    info->isBuiltin = 0;
     info->flag = 0;
+    info->isBuiltin = 0;
+    info->isRedirect = 0;
+    info->num = 1;
     info->cmd = NULL;
     info->input = NULL;
     info->output = NULL;
 }
 
 int
-parseCommand(char *cmdLine, info_t *parsed) {
+parseCommand(char *cmdLine, int *pipe_cmd, info_t *parsed) {
     
     char *ignore = " ";                    // seperate cmds by space
     cmdLine = strtok(cmdLine, "\n");       // get rid of "\n" at the end
@@ -22,16 +24,12 @@ parseCommand(char *cmdLine, info_t *parsed) {
     while (token) {
         parsed->cmd[cnt] = token;
         token = strtok(NULL, ignore);
-        //printf("%s\n", parsed->cmd[cnt]);
         cnt++;
     }
-    //parsed->cmd[cnt] = NULL;
-    /*
-    for (int i=0; i<cnt; i++) {
-        printf("%s ", parsed->cmd[i]);
+    if (cnt == 0) {
+        parsed->num = 0;
+        return -1;
     }
-    printf("\n");
-    */
     char **cmd = parsed->cmd;
     if ((strcmp(cmd[0],Builtin[0])==0) || (strcmp(cmd[0],Builtin[1])==0)
     || (strcmp(cmd[0],Builtin[2])==0) || (strcmp(cmd[0],Builtin[3]))==0)
@@ -41,28 +39,34 @@ parseCommand(char *cmdLine, info_t *parsed) {
         parsed->flag |= Bg;
 
     while (i < cnt) {
+        
+        if (strcmp(cmd[i], "|") == 0) {
+            parsed->flag |= Pp;          // set the "pipe" flag
+            pipe_cmd[parsed->num] = i+1; // record the idx of cmd next to "|" in **cmd
+            parsed->num++;               // increase the command number
+            i += 2;
+            continue;
+        }
         if (strcmp(cmd[i], "<") == 0) {
             parsed->flag |= Ri;
             parsed->input = cmd[i+1];
+            parsed->isRedirect = 1;
             i += 2;
-            if (i >= cnt) break;
+            continue;
         }
         if (strcmp(cmd[i], ">") == 0) { 
             parsed->flag |= Ro;
             parsed->output = cmd[i+1];
+            parsed->isRedirect = 1;
             i += 2;
-            if (i >= cnt) break;
+            continue;
         }
         if (strcmp(cmd[i], ">>") == 0) {
             parsed->flag |= Roo;
             parsed->output = cmd[i+1];
+            parsed->isRedirect = 1;
             i += 2;
-            if (i >= cnt) break;
-        }
-        if (strcmp(cmd[i], "|") == 0) {
-            parsed->flag |= Pp;
-            i++;
-            if (i >= cnt) break;
+            continue;
         }
         else i++;
     }
--- parse.h
+++ parse.h
@@ -14,14 +14,18 @@ void allocation_failed() {
 }
 
 typedef struct Parse_info {
-    int isBuiltin;
-    int flag; //0b(>>)(>)(<)(|)(&) 5-bits representing on/off of these symbols 
+    int flag;         // 0b(>>)(>)(<)(|)(&) 5-bits representing on/off of these symbols 
+    int isRedirect;   // if cmdline includes redirections
+    int isBuiltin;    // if cmd is built-in command
+    int num;          // number of commands, default 1
     char **cmd;
     char *input;
     char *output;
 } info_t;
 
-// function to parse the commands into tokens and set the parse_info
-int parseCommand(char *cmdLine, info_t *parsed);
+void init_parseinfo(info_t *info);
+/* Parsing function to parse the commands into tokens and set the parse_info
+return 0 if success, -1 if cmdLine is empty */
+int parseCommand(char *cmdLine, int *pipe_cmd, info_t *parsed);
 
 #endif
