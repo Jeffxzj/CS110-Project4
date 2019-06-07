8d91797a4ed083afb5050049cbeccbc8c1ca65fb
Made-in-Z <zengli@shanghaitech.edu.cn>
2019-06-07 19:43:11 +0800

    jobs and bg

--- shell.c
+++ shell.c
@@ -15,147 +15,234 @@
 
 const int max_path = 1000;
 const int max_host = 100;
-const int max_line = 100;\
+const int max_line = 100;
+char Running[] = "Running";
+char Done[] = "Done";
+char Terminated[] = "Terminated";
 
 
+typedef struct job_node
+{
+    int job_id;//id [1]
+    int process_id;
+    char* job_src;//copy the command;
+    char* status;//[running] usually
+    struct job_node* next; 
+    struct job_node* prev; 
+    int j;// = 0, if last one, = 1; last second = 2;
+    int need_to_be_del;
+}job_node; 
 
-/*
-    typedef struct job_node
+typedef struct jobs_list 
+{
+    int size;
+    int cap;
+    job_node head;
+    job_node tail;
+    char* status[3];
+}jobs_list;
+jobs_list jobl;
+
+void ini_jobl(){
+	
+	//delete the first several lines in main function
+	jobl.size = 0;
+	jobl.cap = 1;
+	jobl.head.next = &jobl.tail;
+	jobl.tail.prev = &jobl.head;
+	jobl.status[0] = Running;
+	jobl.status[1] = Done;
+	jobl.status[2] = Terminated;
+
+	return;
+}
+
+
+void maintainjobl_j(){
+	if (jobl.head.next == &jobl.tail)
+	{//no job
+		return;
+	}
+	if (jobl.head.next->next == &jobl.tail)
+	{//only one job;
+		jobl.head.next->j = 1;
+		return;		
+	}
+	if (jobl.head.next->next->next == &jobl.tail)
+	{//2 jobs
+		jobl.head.next->next->j = 1;
+		jobl.head.next->j = 2;
+		return;
+	}
+	jobl.tail.prev->j = 1;
+	jobl.tail.prev->prev->j = 2;
+	jobl.tail.prev->prev->prev->j = 0;
+	return;
+}
+
+void addjob(pid_t id, char* command){
+	jobl.size ++;
+	jobl.cap ++;
+	job_node* new_job = (job_node*)malloc(sizeof(job_node));
+	new_job->job_id = jobl.cap;
+	new_job->process_id = id;
+	new_job->prev = jobl.tail.prev;
+	new_job->status = jobl.status[0];
+	jobl.tail.prev->next = new_job;
+	new_job->next = &jobl.tail;
+	new_job->job_src = command;
+	new_job->need_to_be_del = 0;
+	jobl.tail.prev = new_job;
+	new_job->j = 0;
+	maintainjobl_j();
+	return;
+}
+
+void jobs_maintain()
+{//check every job if they have been done.
+	job_node* temp;
+	temp = jobl.head.next;
+	if (jobl.size == 0)
+	{
+		return;
+	}
+	while(temp != &jobl.tail){
+		pid_t pid = temp->process_id;
+		if(temp->need_to_be_del)
+		{//child process ended and need to be deleted
+			job_node* tempprev = temp->prev;
+			jobl.size--;
+			job_node* tempnext = temp->next;
+			tempprev->next = tempnext;
+			tempnext->prev = tempprev;
+			free(temp);
+			maintainjobl_j();
+		}
+		else if (!waitpid(pid, NULL, WNOHANG)&& (temp->need_to_be_del == 0))
+		{
+			temp->need_to_be_del = 1;
+		}
+		temp = temp->next;
+	}
+	
+    return;
+
+}
+
+
+void my_jobs()
+{//first check every pid and maintain the jobl(free)
+	// then maintain j
+	//then print
+	jobs_maintain();
+//printf:
+	job_node* temp;
+	temp = jobl.head.next;
+	if (jobl.size == 0)
+	{
+		return;
+	}
+	while(temp != &jobl.tail){
+		if (temp->j == 0)
+		{
+			printf("[%d]   %s                 %s\n", temp->job_id,temp->status,temp->job_src);
+		}
+		else if(temp->j == 1)//+
+		{
+			printf("[%d]-  %s                 %s\n", temp->job_id,temp->status,temp->job_src);
+		}
+		else if(temp->j == 2)//-
+		{
+			printf("[%d]+  %s                 %s\n", temp->job_id,temp->status,temp->job_src);
+		}
+		temp = temp->next;
+	}
+    return;
+}
+int
+check_process(pid_t processid){
+    job_node* temp;
+    temp = jobl.head.next;
+    if (jobl.size == 0)
     {
-        int job_id;//id [1]
-        int process_id;
-        char* job_src;//copy the command;
-        char* status;//[running] usually
-        job_node* next; 
-        job_node* prev; 
-        int j;// = 0, if last one, = 1; last second = 2;
-    }job_node;
-
-    typedef struct jobs_list 
-    {
-        int size;
-        int cap;
-        job_node head;
-        job_node tail;
-    }jobs_list;
-
-    jobs_list jobl;
-
-
-    int
-    check_process(int processid){
-        job_node* temp;
-        temp = jobl.head.next;
-        if (jobl.size == 0)
-        {
-            return 0;
-        }
-        while(temp != &jobl.tail){
-            if (temp->process_id == processid)
-            {
-                return 1;
-            }
-            temp = temp->next;
-        }
         return 0;
     }
-
-    int  
-    check_job(int num){
-        job_node* temp;
-        temp = jobl.head.next;
-        if (jobl.size == 0)
+    while(temp != &jobl.tail){
+        if (temp->process_id == processid)
         {
-            return 0;
+            return 1;
         }
-        while(temp != &jobl.tail){
-            if (temp->job_id == num)
-            {
-                return 1;
-            }
-            temp = temp->next;
-        }
+        temp = temp->next;
+    }
+    return 0;
+}
+
+int  
+check_job(int num){
+    job_node* temp;
+    temp = jobl.head.next;
+    if (jobl.size == 0)
+    {
         return 0;
     }
-
-    void kill_process(int processid){
-        //kill 6666
-        job_node* temp;
-        job_node* temp_prev;
-        job_node* temp_next;
-        temp = jobl.head.next;
-        if (jobl.size == 0)
+    while(temp != &jobl.tail){
+        if (temp->job_id == num)
         {
+            return 1;
+        }
+        temp = temp->next;
+    }
+    return 0;
+}
+
+
+void kill_process(pid_t processid){
+    //kill 6666
+    job_node* temp;
+    temp = jobl.head.next;
+    if(!check_process(processid)){
+    	return;
+    }
+    while(temp != &jobl.tail){
+        if (temp->process_id == processid)
+    	{
+        	if (!strcmp(Running,temp->status))
+        	{
+        		//kill it;
+        		kill(temp->process_id,SIGKILL);
+        		temp->status = Terminated;
+        	}
             return;
         }
-        while(temp != &jobl.tail){
-            if (temp->process_id == processid)
-            {
-                temp_prev = temp->prev;
-                temp_next = temp->next;
-                temp_prev->next = temp_next;
-                temp_next->prev = temp_prev;
-                jobl.size--;
-                free(temp);
-                return;
-            }
-            temp = temp->next;
-        }
+        temp = temp->next;
     }
+    return;
+}
 
-    void
-    kill_num(int num){
-        //kill %num 
-        //previous to this, it has been checked that if this process exists
-        job_node* temp;
-        job_node* temp_prev;
-        job_node* temp_next;
-        temp = jobl.head.next;
-        if (jobl.size == 0)
-        {
+void
+kill_num(int num){
+    //kill %num 
+    //previous to this, it has been checked that if this process exists
+    if (!check_job(num))
+    {
+    	return;
+    }        
+    job_node* temp;
+    temp = jobl.head.next;
+    while(temp != &jobl.tail){
+        if (temp->job_id == num)
+    	{
+        	if (!strcmp(Running,temp->status))
+        	{
+        		//kill it;
+        		kill(temp->process_id,SIGKILL);
+        		temp->status = Terminated;
+        	}
             return;
         }
-        while(temp != &jobl.tail){
-            if (temp->job_id == num)
-            {
-                temp_prev = temp->prev;
-                temp_next = temp->next;
-                temp_prev->next = temp_next;
-                temp_next->prev = temp_prev;
-                jobl.size--;
-                free(temp);
-                return;
-            }
-            temp = temp->next;
-        }
+        temp = temp->next;
     }
-
-
-    void 
-    my_jobs(){
-        job_node* temp;
-        temp = jobl.head.next;
-        if (jobl.size == 0)
-        {
-            return;
-        }
-        while(temp != &jobl.tail){
-            if (temp->j == 0)
-            {
-                printf("[%d]   %s                 %s\n", temp->job_id,temp->status,temp->job_src);
-            }
-            else if(temp->j == 1)//+
-            {
-                printf("[%d]-  %s                 %s\n", temp->job_id,temp->status,temp->job_src);
-            }
-            else if(temp->j == 2)//-
-            {
-                printf("[%d]+  %s                 %s\n", temp->job_id,temp->status,temp->job_src);
-            }
-            temp = temp->next;
-        }
-    }
-*/
+}
 
 /*
     void 
@@ -197,7 +284,9 @@ executeBuiltInCommand(char **cmd) {
     */
     // Command "jobs"
     if (strcmp(cmd[0], Builtin[2]) == 0){
-    	//my_jobs();
+        printf("my_jobs\n");
+    	my_jobs();
+        printf("my_jobsend\n");
         return 1;
     }
  
@@ -214,7 +303,6 @@ void
 executePipes(char **cmd, int *pipe_idx, int cmd_cnt) 
 {
     // pipe number is always 1 less than cmd number
-    int stat_loc;
     int pipe_cnt = cmd_cnt - 1; 
     int pipefd[2*pipe_cnt]; 
     pid_t childpid;
@@ -249,8 +337,7 @@ executePipes(char **cmd, int *pipe_idx, int cmd_cnt)
     for (int i=0; i<2*pipe_cnt; i++)
         close(pipefd[i]);
     for (int i=0; i<cmd_cnt; i++)
-        waitpid(childpid, NULL, 0);
-    //exit(0);
+        waitpid(childpid, NULL, WUNTRACED);
 
 }
 
@@ -294,23 +381,19 @@ cmdPrinter(char **cmd)
 
 int 
 main (int argc, char **argv)
-{
-	//int flag = 0;
-    //jobl.size = 0;        
+{       
     FILE *fp;
-    //if (argc > 1) {
-    //printf("%s",argv[1]);
     fp = fopen(argv[1], "r"); // open the .sh shell script
-    
+    ini_jobl();
     char *cmdLine, **cmd;
     int *pipe_idx;
     // cmd tokens after parsing
     cmd = malloc(max_line * sizeof(char*)); 
     // piping commands indexs in cmd
     pipe_idx = malloc(max_line * sizeof(int));
-    pid_t childPid;
     
     while (!feof(fp)){
+        pid_t childPid;
         // initialize the parse_info
         info_t parsed;
         init_parseinfo(&parsed);
@@ -333,41 +416,45 @@ main (int argc, char **argv)
             continue;
         }
         childPid = fork();
-
-
-        // for the child process
+        // for the new process
         if (childPid == 0){
             // Only redirection, no pipes
             if (parsed.isRedirect && !(parsed.flag & Pp)) {
                 executeRedirections(&parsed);
                 continue;
             }
+            // if piped
             if (parsed.flag & Pp) {
+/*
+                printf("%d\n",parsed.num);
+                for (int k=0; k<parsed.num; k++){
+                    printf("%d ",pipe_idx[k]);
+                    printf("\n");
+                }
+*/
                 executePipes(cmd, pipe_idx, parsed.num);
-                break;
+                continue;
             }
             execvp(cmd[0], cmd); //calls execvp() to execute the cmd
         }
-        else
-            waitpid(childPid, NULL, 0);
-        
-        // for the parent process
-        /*
         else {
-            // if piped
-            if (parsed.flag & Pp) {
-                executePipes(cmd, pipe_idx, parsed.num);
-                //waitpid(childPid, NULL, 0);
-
-                continue;
-            }
-            waitpid(childPid, NULL, 0);
-        }
-        */		
+            // if the command is background
+            
+            if (parsed.flag & Bg) {
+                //printf("background jobs\n");
+                //record in list of background jobs
+                
+                addjob(childPid,cmdLine);
+                execvp(cmd[0], cmd);
+            } else {
+            
+            waitpid(childPid, NULL, WUNTRACED);
+        }		
     }
     free(cmd);
     free(cmdLine);
     free(pipe_idx);
     fclose(fp);
     return 0;
+}
 }
